4db139e7d55393ea34b70aead8737969287e8d1f
Dan Brady <dannymb360@gmail.com>
2023-06-27 16:58:12 -0400

    category

--- TabloidMVC/Repositories/ICategoryRepository.cs
+++ TabloidMVC/Repositories/ICategoryRepository.cs
@@ -6,5 +6,13 @@ namespace TabloidMVC.Repositories
     public interface ICategoryRepository
     {
         List<Category> GetAll();
+
+        Category GetById(int id);
+
+        void Add(Category category);
+
+        void Update(Category category); 
+
+        void Delete(int id);
     }
 }
