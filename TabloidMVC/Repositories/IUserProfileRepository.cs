70f488ff9a2e517d106481816c7a0a42390671ea
Dan Brady <dannymb360@gmail.com>
2023-06-28 15:56:09 -0400

    users

--- TabloidMVC/Repositories/IUserProfileRepository.cs
+++ TabloidMVC/Repositories/IUserProfileRepository.cs
@@ -1,9 +1,16 @@
 ﻿using TabloidMVC.Models;
+using System.Collections.Generic;
 
 namespace TabloidMVC.Repositories
 {
     public interface IUserProfileRepository
     {
         UserProfile GetByEmail(string email);
+
+        List<UserProfile> GetAll();
+
+        UserProfile GetById(int id);
+
+        void DeactivateUser(int id);
     }
 }
