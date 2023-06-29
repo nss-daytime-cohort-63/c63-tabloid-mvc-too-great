997dfad5b3b1191731b333b6e446fcd1b201168a
Adam Riggs <adambolan31@gmail.com>
2023-06-29 09:52:08 -0500

    commit for tnew main

---
+++ TabloidMVC/Repositories/ITagRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public interface ITagRepository
+    {
+        List<Tag> GetAll();
+
+        Tag GetTagById(int id);
+        void AddTag(Tag tag);
+
+        void UpdateTag(Tag tag);
+
+        void DeleteTag(int tagId);
+
+    }
+}
